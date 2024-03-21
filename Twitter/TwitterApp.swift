d5e0c1f38e2570db6febd12f0ccb59269f3e8b9e
Tanmoy07072001 <roytanmoy07072001@gmail.com>
2024-03-21 10:30:15 +0530

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by TANMOY ROY on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
